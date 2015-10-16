64cb1b59ec04fd2bfe8234fbb0d7d9232d97a176
juaning <juaning@gmail.com>
2015-10-16 21:08:53 +1100

    Add form and js functionalities for dates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-var startDate = "2013-04-27";
-var holidays = new Array("2013-04-28","2013-05-22","2013-06-28");
+var startDate = "2013-05-21";
+var holidays = new Array("2015-10-19","2015-10-22","2015-10-28");
+var DATEFORMAT = 'dd/mm/yy';
 
 /**
  * Converts String Date to Object Date
@@ -22,19 +23,48 @@ function convertHolidaysDatesToObj(holidays) {
   })
 }
 
+/**
+ * Calculate Date from Starting Date & Number of Days
+ * @param  {[type]} startDate [description]
+ * @param  {[type]} numOfDays [description]
+ * @param  {[type]} holidays  [description]
+ * @return {[type]}           [description]
+ */
 function calculateDate(startDate, numOfDays, holidays) {
   var endDate = '', count = 0;
   while(count < numOfDays){
       endDate = new Date(startDate.setDate(startDate.getDate() + 1));
-      if(endDate.getDay() != 0 && endDate.getDay() != 6 && holidays.indexOf(endDate) !== -1){
-         //Date.getDay() gives weekday starting from 0(Sunday) to 6(Saturday)
+      if(endDate.getDay() != 0 && endDate.getDay() != 6
+        && holidays.map(Number).indexOf(+endDate) < 0){
          count++;
       }
   }
   return endDate;
 }
 
-var objDate = convertDateStrToDateObj(startDate);
+// var objDate = convertDateStrToDateObj(startDate);
 var arrObjHolidays = convertHolidaysDatesToObj(holidays);
+//
+// console.log(calculateDate(objDate, 1, arrObjHolidays));
 
-console.log(calculateDate(objDate, 1, arrObjHolidays));
+$.datepicker.setDefaults({
+  dateFormat: DATEFORMAT
+});
+
+$(function(){
+  $('#date').datepicker();
+
+  $(document).on('submit', '#calculate-form', function(e){
+    e.preventDefault();
+  });
+
+  $(document).on('click', '.btn-default', function(e) {
+    e.preventDefault();
+    console.log('Clicked', $('#date').val(), $('#days').val(), DATEFORMAT);
+    var arrDate = $('#date').val().split('/');
+    var startDate = convertDateStrToDateObj(arrDate[2] + '-' + arrDate[1] + '-' + arrDate[0]);
+    console.log('startDate', startDate);
+    var result = 'Business day is: ' + calculateDate(startDate, $('#days').val(), arrObjHolidays)
+    $('#date-result').html(result);
+  })
+})
